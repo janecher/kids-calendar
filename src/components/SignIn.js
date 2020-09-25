f89beac9ca9604f7efaba2c4829a73ba77c70966
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-09-25 13:26:42 -0700

    add PropTypes

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 function SignIn(props){
   return (
@@ -19,5 +20,9 @@ function SignIn(props){
       </React.Fragment> 
   );
 }
+
+SignIn.propTypes = {
+  onClickingSignUp: PropTypes.func
+};
 
 export default SignIn;
