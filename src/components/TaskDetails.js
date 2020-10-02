a2ea67e31b95fb2831e0c78963738b74ca0eff0b
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-10-02 14:52:15 -0700

    add styling and structure to TaskDetail component

--- src/components/TaskDetails.js
+++ src/components/TaskDetails.js
@@ -4,17 +4,30 @@ import PropTypes from "prop-types";
 function TaskDetail(props){
   return (
     <React.Fragment>
-        <p>{props.task}</p>
-        <p>{props.startTime}</p>
-        <button type='button'>Edit</button>
-        <button type='button'>Done</button>
-        <button type='button'>Delete</button>
+      <div className="col-md-8 col-lg-6 mt-5 mx-auto start-page fill">
+        <div className="card">
+          <div className="card-body">
+            <button type="button" onClick = {() => props.onCloseTaskDetail()} className="close" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+            <h4 className="card-title">{props.formHeader}</h4>
+            <div className="card-text">
+              <p>Name: {props.task.name}</p>
+              <p>Start time: {props.task.startTime}</p>
+              <p>End time: {props.task.startTime}</p>
+              <button type='button' className="btn btn-info">Edit</button>
+              <button type='button' className="btn btn-info">Done</button>
+              <button type='button' className="btn btn-info">Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
 
 TaskDetail.propTypes = {
-  task: PropTypes.string,
-  startTime: PropTypes.string
+  task: PropTypes.object,
+  onCloseTaskDetail: PropTypes.func
 };
 export default TaskDetail
