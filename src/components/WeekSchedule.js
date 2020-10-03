1232b9affb330f19980f32c0770eeb71109c19e5
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-10-02 22:11:13 -0700

    add functionality to show tasks on WeekSchedule

--- src/components/WeekSchedule.js
+++ src/components/WeekSchedule.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import WeekDay from './WeekDay';
 import PropTypes from "prop-types";
+import { useSelector } from 'react-redux'
+import { useFirestoreConnect, isLoaded, isEmpty } from 'react-redux-firebase'
 
 function WeekSchedule(props){
   const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
+  useFirestoreConnect([
+    { collection: 'tasks' }
+  ]);
+
+  const tasks = useSelector(state => state.firestore.ordered.tasks);
+
   const getTasksForDay = (dayName) => {
     let tasksForDay = [];
-    props.tasks.forEach(task => {
+    tasks.forEach(task => {
       if (task.day === dayName) {
         tasksForDay.push(task);
       }
@@ -15,15 +23,27 @@ function WeekSchedule(props){
     return tasksForDay;
   }
 
-  return (
-    <React.Fragment>
+  if (isLoaded(tasks) && !isEmpty(tasks)) {
+    return (
+      <React.Fragment>
       <div className="row text-center rounded">
         {weekDays.map((weekday, index) =>
           <WeekDay day={weekday} key={index} tasks={getTasksForDay(weekday)} whenTaskClicked = {props.onTaskSelection}/>
         )}
       </div>
       </React.Fragment>
-  );
+    );
+  } else {
+    return (
+      <React.Fragment>
+      <div className="row text-center rounded">
+        {weekDays.map((weekday, index) =>
+          <WeekDay day={weekday} key={index} tasks={[]}/>
+        )}
+      </div>
+      </React.Fragment>
+    );
+  }
 }
 
 WeekSchedule.propTypes = {
