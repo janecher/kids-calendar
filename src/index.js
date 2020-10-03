93a90c9c3f4f1a9e4fe2ea50a6a23e020bd26bd3
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-10-02 20:24:33 -0700

    add firestore to project, reducers

--- src/index.js
+++ src/index.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index-reducer';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
+import 'firebase/auth';
+
+const store = createStore(rootReducer);
+
+const rrfProps = {
+  firebase,
+  config: {
+        userProfile: "users",
+        useFirestoreForProfile: true,
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
