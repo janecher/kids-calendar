9bce95b2709d9eb1bf1a3059e0fa61bdeff05bd4
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-10-07 08:34:18 -0700

    add routes to project

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 import SignIn from './SignIn';
 import SignUpForm from './SignUpForm';
 import UserControl from './UserControl';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-  const [signUpForm, setSignUpForm] = useState(false);
-
-  const toggleSignUp = () => {
-    setSignUpForm(!signUpForm);
-  }
-
-  let currentPage = null;
-
-  if(signUpForm) {
-    currentPage = <SignUpForm onCloseSignUpForm={toggleSignUp}/>
-  } else {
-    currentPage = <SignIn onClickingSignUp={toggleSignUp}/>
-  }
-
-  currentPage = <UserControl />
-  return (
-    <React.Fragment>
-      {currentPage}
-    </React.Fragment>
+  return ( 
+    <Router>
+      <Switch>
+        <Route path="/signup">
+          <SignUpForm />
+        </Route>
+        <Route path="/signin">
+          <SignIn />
+        </Route>
+        <Route path="/">
+          <UserControl />
+        </Route>
+      </Switch>
+    </Router>
   );
+  // const [signUpForm, setSignUpForm] = useState(false);
+
+  // const toggleSignUp = () => {
+  //   setSignUpForm(!signUpForm);
+  // }
+
+  // let currentPage = null;
+
+  // if(signUpForm) {
+  //   currentPage = <SignUpForm onCloseSignUpForm={toggleSignUp}/>
+  // } else {
+  //   currentPage = <SignIn onClickingSignUp={toggleSignUp}/>
+  // }
+
+  // currentPage = <UserControl />
+  // return (
+  //   <React.Fragment>
+  //     {currentPage}
+  //   </React.Fragment>
+  // );
 }
 
 export default App;
