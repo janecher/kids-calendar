5a71290e3e9c9efa53a165b5038df6032a51ed3a
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-10-07 12:01:45 -0700

    add authentication, add functionality to show users name and sign in user stickers

--- src/components/UserControl.js
+++ src/components/UserControl.js
@@ -6,8 +6,16 @@ import TaskDetail from './TaskDetails';
 import { withFirestore, useFirestoreConnect, isLoaded, isEmpty} from 'react-redux-firebase';
 import { useSelector } from 'react-redux';
 import StickersPage from './StickersPage';
+import firebase from "firebase/app";
+import SignIn from './SignIn';
 
 function UserControl(props){
+
+  const auth = props.firebase.auth();
+
+  const name = auth.currentUser ? auth.currentUser.displayName : "Name";
+
+  const userId = auth.currentUser ? auth.currentUser.uid : null;
 
   //array for weekday names
   const weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
@@ -80,7 +88,8 @@ function UserControl(props){
         endTime: task.get("endTime"),
         day: task.get("day"),
         isDone: task.get("isDone"),
-        id: task.id
+        id: task.id,
+        userId: task.userId
       }
       setSelectedTask(firestoreTask);
     });    
@@ -97,6 +106,15 @@ function UserControl(props){
     return Math.floor(Math.random() * Math.floor(border));
   }
 
+  //signOut
+  function doSignOut() {
+    firebase.auth().signOut().then(function() {
+      window.location = 'home.html';
+    }).catch(function(error) {
+      alert(error.message);
+    });
+  }
+
   //calculation what is the day of the week
   let todayIndex = new Date().getDay();
   let today = todayIndex === 0 ? weekDays[6] : weekDays[todayIndex-1]
@@ -105,18 +123,21 @@ function UserControl(props){
   useFirestoreConnect([
     { collection: 'stickers' }
   ]);
-
-  const stickers = useSelector(state => state.firestore.ordered.stickers);
+  
+  const allStickers = useSelector(state => state.firestore.ordered.stickers);
+  //get only stickers of auth user
+  const stickers = allStickers ? allStickers.filter(sticker => sticker.userId === userId) : [];
 
   //set which component to show
   let currentPage = null;
 
   if(stickersPage) {
-    if(isLoaded(stickers) && !isEmpty(stickers)) {
-      currentPage = <StickersPage stickers = {stickers} onClickingStickersPageClose = {toggleStickersPage}/>
-    } else {
-      currentPage = <StickersPage stickers = {[]} onClickingStickersPageClose = {toggleStickersPage}/>
-    }
+    currentPage = <StickersPage stickers = {stickers} onClickingStickersPageClose = {toggleStickersPage}/>
+    // if(isLoaded(stickers) && !isEmpty(stickers)) {
+    //   currentPage = <StickersPage stickers = {stickers} onClickingStickersPageClose = {toggleStickersPage}/>
+    // } else {
+    //   currentPage = <StickersPage stickers = {[]} onClickingStickersPageClose = {toggleStickersPage}/>
+    // }
   } else if(editTaskForm) {
     currentPage = <EditTaskForm 
                     task = {selectedTask} 
@@ -130,37 +151,54 @@ function UserControl(props){
                     onClickingDelete = {handleDeletingTask} 
                     onClickingDoneQuote = {showQuotes}
                     numberOfStickers = {stickers ? stickers.length : 0} 
+                    userId = {userId}
                     />
   } else if(addTaskForm) {
-    currentPage = <AddTaskForm onCloseAddTaskForm={closeAddTaskForm} onNewTaskCreation={handleAddingNewTask} />
+    currentPage = <AddTaskForm onCloseAddTaskForm={closeAddTaskForm} onNewTaskCreation={handleAddingNewTask} userId={userId}/>
   } else {
-    currentPage = <WeekSchedule onTaskSelection={handleChangingSelectedTask} weekDays = {weekDays} colors = {colors}/>
+    currentPage = <WeekSchedule onTaskSelection={handleChangingSelectedTask} weekDays = {weekDays} colors = {colors} userId={userId}/>
   }
 
-  return (
-    <React.Fragment>
-      <div className="row text-center">
-        <div className="col-md-2 col-xs-12 col-sm-6 order-md-1 order-sm-1 hello p-3 border-right border-bottom border-info light-background">
-          <h2>Name</h2>
+  if (!isLoaded(auth)) {
+    return (
+      <React.Fragment>
+        <h1>Loading...</h1>
+      </React.Fragment>
+    )
+  }
+  if ((isLoaded(auth)) && (auth.currentUser == null)) {
+    return (
+      <React.Fragment>
+        <SignIn />
+      </React.Fragment>
+    )
+  } 
+  if ((isLoaded(auth)) && (auth.currentUser != null)) {
+    return (
+      <React.Fragment>
+        <div className="row text-center">
+          <div className="col-md-2 col-xs-12 col-sm-6 order-md-1 order-sm-1 hello p-3 border-right border-bottom border-info light-background">
+            <h4>{name}</h4>
+          </div>
+          <div className="col-md-8 col-xs-12 col-sm-12 order-md-1 order-sm-2 p-3 border-bottom border-info light-background">
+            <h2>Today: <span className="pl-3">{today}</span></h2>
+          </div>
+          <div className="col-md-2 col-xs-12  col-sm-6 order-sm-1 p-3 border-bottom border-info light-background">
+            <button type="button" onClick={doSignOut} className="btn btn-info btn-lg">Sign Out</button>
+          </div>
         </div>
-        <div className="col-md-8 col-xs-12 col-sm-12 order-md-1 order-sm-2 p-3 border-bottom border-info light-background">
-          <h2>Today: {today}</h2>
+        <div className="row text-center">
+          <div className="col-md-2 col-sm-12 col-xs-12 sidebar border-right border-bottom border-info light-background">
+            <button type="button" className="btn btn-info btn-lg mt-3 mb-3" onClick={toggleAddTaskForm}>Add task</button>
+            <button type="button" className="btn btn-info btn-lg mt-3 mb-3" onClick={toggleStickersPage}>Treasures</button>
+            <h4>{quote}</h4>
+          </div>
+          <div className={"col-md-10 " + colors[getRandomNumber(colors.length)]}>
+            {currentPage}
+          </div>
         </div>
-        <div className="col-md-2 col-xs-12  col-sm-6 order-sm-1 p-3 border-bottom border-info light-background">
-          <button type="button" className="btn btn-info btn-lg">Sign Out</button>
-        </div>
-      </div>
-      <div className="row text-center">
-        <div className="col-md-2 col-sm-12 col-xs-12 sidebar border-right border-bottom border-info light-background">
-          <button type="button" className="btn btn-info btn-lg mt-3 mb-3" onClick={toggleAddTaskForm}>Add task</button>
-          <button type="button" className="btn btn-info btn-lg mt-3 mb-3" onClick={toggleStickersPage}>Treasurs</button>
-          <h4>{quote}</h4>
-        </div>
-        <div className={"col-md-10 " + colors[getRandomNumber(colors.length)]}>
-          {currentPage}
-        </div>
-      </div>
-    </React.Fragment>
-  );
+      </React.Fragment>
+    );
+  }
 }
 export default withFirestore(UserControl);
